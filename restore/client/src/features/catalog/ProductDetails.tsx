18654006bf65095d05e7020d731455b6a39735c1
Stuart Murray <stuart.murray.dev@outlook.com>
2023-04-04 17:30:53 +0100

    start of styled details page

--- restore/client/src/features/catalog/ProductDetails.tsx
+++ restore/client/src/features/catalog/ProductDetails.tsx
@@ -20,7 +20,69 @@ function ProductDetails() {
 
   if (!product) return <h3>Product not found</h3>;
 
-  return <h2>{product.name}</h2>;
+  return (
+    <div className='row'>
+      <div className='col-6'>
+        <img
+          src={product.pictureUrl}
+          alt={product.name}
+          style={{ width: '100%' }}
+        />
+      </div>
+      <div className='col-6'>
+        <h3 style={{ fontWeight: 'bold', fontSize: '2.5em' }}>
+          {product.name}
+        </h3>
+        <hr className='mb-2' />
+        <h4
+          style={{ fontWeight: 'bold', fontSize: '1.5em' }}
+          className='text-primary'
+        >
+          ${(product.price / 100).toFixed(2)}
+        </h4>
+        <div className='row'>
+          <div className='col-4'>
+            <div style={{ fontWeight: 'bolder' }}>Name</div>
+          </div>
+          <div className='col-8'>
+            <div>{product.name}</div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-4'>
+            <div style={{ fontWeight: 'bolder' }}>Description</div>
+          </div>
+          <div className='col-8'>
+            <div>{product.description}</div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-4'>
+            <div style={{ fontWeight: 'bolder' }}>Type</div>
+          </div>
+          <div className='col-8'>
+            <div>{product.type}</div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-4'>
+            <div style={{ fontWeight: 'bolder' }}>Brand</div>
+          </div>
+          <div className='col-8'>
+            <div>{product.brand}</div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-4'>
+            <div style={{ fontWeight: 'bolder' }}>Quantity in Stock</div>
+          </div>
+          <div className='col-8'>
+            <div>{product.quantityInStock}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductDetails;
