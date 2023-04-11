88224f40e36cf9c742fdd3a8b3f38b805e4479ea
Stuart Murray <stuart.murray.dev@outlook.com>
2023-04-11 14:29:25 +0100

    move axios centrally

--- restore/client/src/features/catalog/Catalog.tsx
+++ restore/client/src/features/catalog/Catalog.tsx
@@ -1,19 +1,16 @@
 import { useEffect, useState } from 'react';
 import { Product } from '../../app/models/product';
 import ProductList from './ProductList';
+import agent from '../../app/api/agent';
 
 function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+    agent.Catalog.list().then((products) => setProducts(products));
   }, []);
 
-  return (
-      <ProductList products={products} />
-  );
+  return <ProductList products={products} />;
 }
 
 export default Catalog;
--- restore/client/src/features/catalog/ProductDetails.tsx
+++ restore/client/src/features/catalog/ProductDetails.tsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Product } from '../../app/models/product';
+import agent from '../../app/api/agent';
 
 function ProductDetails() {
   const { id } = useParams<{ id: string }>();
@@ -9,11 +9,11 @@ function ProductDetails() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get<Product>(`http://localhost:5000/api/products/${id}`)
-      .then((response) => setProduct(response.data))
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
+    id &&
+      agent.Catalog.details(parseInt(id))
+        .then((response) => setProduct(response))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
   }, [id]);
 
   if (loading) return <h3>Loading...</h3>;
