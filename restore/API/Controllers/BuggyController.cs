eb80098178516cd8daa23ea5bf574a8761d47426
Stuart Murray <stuart.murray.dev@outlook.com>
2023-04-11 13:31:07 +0100

    Move to BaseApiController class and create BuggyController

---
+++ restore/API/Controllers/BuggyController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ProblemDetails { Title = "this is a bad request" });
+        }
+
+        [HttpGet("unathorised")]
+        public ActionResult GetUnauthorised()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("validation-error")]
+        public ActionResult GetValidationError()
+        {
+            ModelState.AddModelError("Problem1", "this is the first error");
+            ModelState.AddModelError("Problem2", "this is the second error");
+            return ValidationProblem();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("this is a server error");
+        }
+    }
+}
