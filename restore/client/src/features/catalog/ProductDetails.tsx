e1b656d0f3aff1394599b156ccb71086e4d4a148
Stuart Murray <stuart.murray.dev@outlook.com>
2023-05-03 12:26:51 +0100

    add quantity and quantity change to peoduct details

--- restore/client/src/features/catalog/ProductDetails.tsx
+++ restore/client/src/features/catalog/ProductDetails.tsx
@@ -5,19 +5,47 @@ import agent from '../../app/api/agent';
 import NotFound from '../../app/errors/NotFound';
 import LoadingComponent from '../../app/layout/LoadingComponent';
 import { currencyFormat } from '../../app/util/util';
+import { useStoreContext } from '../../app/context/StoreContext';
+import { Form, Spinner } from 'react-bootstrap';
 
 function ProductDetails() {
+  const { basket, setBasket, removeItem } = useStoreContext();
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
+  const [quantity, setQuantity] = useState(0);
+  const [submitting, setSubmitting] = useState(false);
+  const item = basket?.items.find((i) => i.productId === product?.id);
 
   useEffect(() => {
+    if (item) setQuantity(item.quantity);
     id &&
       agent.Catalog.details(parseInt(id))
         .then((response) => setProduct(response))
         .catch((error) => console.log(error))
         .finally(() => setLoading(false));
-  }, [id]);
+  }, [id, item]);
+
+  function handleInputChange(event: any) {
+    if (event.target.value > 0) setQuantity(parseInt(event.target.value));
+  }
+
+  function handleUpdateCart() {
+    setSubmitting(true);
+    if (!item || quantity > item.quantity) {
+      const updatedQuantity = item ? quantity - item.quantity : quantity;
+      agent.Basket.addItem(product?.id!, updatedQuantity)
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setSubmitting(false));
+    } else {
+      const updatedQuantity = item.quantity - quantity;
+      agent.Basket.removeItem(product?.id!, updatedQuantity)
+        .then(() => removeItem(product?.id!, updatedQuantity))
+        .catch((error) => console.log(error))
+        .finally(() => setSubmitting(false));
+    }
+  }
 
   if (loading) return <LoadingComponent message='Loading product...' />;
 
@@ -81,6 +109,37 @@ function ProductDetails() {
           </div>
           <div className='col-8'>
             <div>{product.quantityInStock}</div>
+          </div>
+        </div>
+        <div className='row mt-3'>
+          <div className='col-6'>
+            <Form.Group controlId='formBasicQuantity'>
+              <Form.Label>Quantity in cart</Form.Label>
+              <Form.Control
+                type='number'
+                placeholder='Quantity'
+                value={quantity}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+          </div>
+          <div className='col-6'>
+            <button
+              className='btn btn-primary w-100'
+              style={{ height: '40px', marginTop: '30px' }}
+              onClick={handleUpdateCart}
+            >
+              {submitting && (
+                <Spinner
+                  as='span'
+                  animation='border'
+                  size='sm'
+                  role='status'
+                  aria-hidden='true'
+                />
+              )}
+              {item ? 'Update Quantity' : 'Add to cart'}
+            </button>
           </div>
         </div>
       </div>
