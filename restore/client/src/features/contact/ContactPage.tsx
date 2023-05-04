5261f3fe3ad885cb5493ba88070fc6c21d0c77f1
Stuart Murray <stuart.murray.dev@outlook.com>
2023-05-04 10:23:32 +0100

    redux toolkit version

--- restore/client/src/features/contact/ContactPage.tsx
+++ restore/client/src/features/contact/ContactPage.tsx
@@ -1,25 +1,21 @@
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  CounterState,
-  decrement,
-  increment,
-} from './counterReducer';
 import { Fragment } from 'react';
 import { Button, ButtonGroup } from 'react-bootstrap';
+import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
+import { decrement, increment } from './counterSlice';
 
 function ContactPage() {
-  const dispatch = useDispatch();
-  const { data, title } = useSelector((state: CounterState) => state);
+  const dispatch = useAppDispatch();
+  const { data, title } = useAppSelector((state) => state.counter);
 
   return (
     <Fragment>
       <h2>{title}</h2>
       <h5>The data is: {data}</h5>
       <ButtonGroup>
-        <Button variant='danger' onClick={() => dispatch(decrement())}>
+        <Button variant='danger' onClick={() => dispatch(decrement(1))}>
           Decrement
         </Button>
-        <Button variant='primary' onClick={() => dispatch(increment())}>
+        <Button variant='primary' onClick={() => dispatch(increment(1))}>
           Increment
         </Button>
         <Button variant='secondary' onClick={() => dispatch(increment(5))}>
--- restore/client/src/index.tsx
+++ restore/client/src/index.tsx
@@ -7,10 +7,8 @@ import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
 import { StoreProvider } from './app/context/StoreContext';
-import { configureStore } from './app/store/configureStore';
 import { Provider } from 'react-redux';
-
-const store = configureStore();
+import { store } from './app/store/configureStore';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
