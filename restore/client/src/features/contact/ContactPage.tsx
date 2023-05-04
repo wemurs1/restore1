805760a224db519b40652d220b4ff5759926a4db
Stuart Murray <stuart.murray.dev@outlook.com>
2023-05-04 09:28:21 +0100

    basic redux

--- restore/client/src/features/contact/ContactPage.tsx
+++ restore/client/src/features/contact/ContactPage.tsx
@@ -1,7 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  CounterState,
+  decrement,
+  increment,
+} from './counterReducer';
+import { Fragment } from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
+
 function ContactPage() {
-    return (
-        <h2>Contact page</h2>
-    )
+  const dispatch = useDispatch();
+  const { data, title } = useSelector((state: CounterState) => state);
+
+  return (
+    <Fragment>
+      <h2>{title}</h2>
+      <h5>The data is: {data}</h5>
+      <ButtonGroup>
+        <Button variant='danger' onClick={() => dispatch(decrement())}>
+          Decrement
+        </Button>
+        <Button variant='primary' onClick={() => dispatch(increment())}>
+          Increment
+        </Button>
+        <Button variant='secondary' onClick={() => dispatch(increment(5))}>
+          Increment 5
+        </Button>
+      </ButtonGroup>
+    </Fragment>
+  );
 }
 
 export default ContactPage;
--- restore/client/src/index.tsx
+++ restore/client/src/index.tsx
@@ -7,6 +7,10 @@ import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
 import { StoreProvider } from './app/context/StoreContext';
+import { configureStore } from './app/store/configureStore';
+import { Provider } from 'react-redux';
+
+const store = configureStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,7 +18,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <StoreProvider>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </StoreProvider>
   </React.StrictMode>
 );
