e149c3985db24a1904d9eb89841ee5be05294343
Stuart Murray <stuart.murray.dev@outlook.com>
2023-05-22 18:21:02 +0100

    checkpoint

---
+++ restore/API/Controllers/OrdersController.cs
@@ -0,0 +1,108 @@
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Entities.OrderAggregate;
+using API.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class OrdersController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public OrdersController(StoreContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Order>>> GetOrders()
+        {
+            return await _context.Orders
+                .Include(o => o.Orderitems)
+                .Where(x => x.BuyerId == User.Identity!.Name)
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}", Name = "GetOrder")]
+        public async Task<ActionResult<Order?>> GetOrder(int id)
+        {
+            return await _context.Orders
+                .Include(o => o.Orderitems)
+                .Where(x => x.BuyerId == User.Identity!.Name && x.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateOrder(CreateOrderDto orderDto)
+        {
+            var basket = await _context.Baskets.RetrieveBasketWithItems(User.Identity!.Name!).FirstOrDefaultAsync();
+
+            if (basket == null) return BadRequest(new ProblemDetails { Title = "Could not locate basket" });
+
+            var items = new List<OrderItem>();
+
+            foreach (var item in basket.Items)
+            {
+                var productItem = await _context.Products.FindAsync(item.ProductId);
+                if (productItem == null) return BadRequest(new ProblemDetails { Title = "Product in basket not found" });
+                var itemOrdered = new ProductItemOrdered
+                {
+                    ProductId = productItem.Id,
+                    Name = productItem.Name,
+                    PictureUrl = productItem.PictureUrl
+                };
+                var orderItem = new OrderItem
+                {
+                    ItemOrdered = itemOrdered,
+                    Price = productItem.Price,
+                    Quantity = item.Quantity
+                };
+                items.Add(orderItem);
+                productItem.QuantityInStock -= item.Quantity;
+            }
+
+            var subTotal = items.Sum(item => item.Price * item.Quantity);
+            var deliveryFee = subTotal > 100000 ? 0 : 500;
+
+            var order = new Order
+            {
+                Orderitems = items,
+                BuyerId = User.Identity.Name,
+                ShippingAddress = orderDto.ShippingAddress,
+                SubTotal = subTotal,
+                DeliveryFee = deliveryFee
+            };
+
+            _context.Orders.Add(order);
+            _context.Baskets.Remove(basket);
+
+            if (orderDto.SaveAddress)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
+                if (user == null) return BadRequest(new ProblemDetails { Title = "Can't find user record" });
+                user.Address = new UserAddress
+                {
+                    FullName = orderDto.ShippingAddress!.FullName,
+                    Address1 = orderDto.ShippingAddress.Address1,
+                    Address2 = orderDto.ShippingAddress.Address2,
+                    City = orderDto.ShippingAddress.City,
+                    State = orderDto.ShippingAddress.State,
+                    Zip = orderDto.ShippingAddress.Zip,
+                    Country = orderDto.ShippingAddress.Country
+                };
+                _context.Update(user);
+            }
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return CreatedAtRoute("GetOrder", new { id = order.Id }, order.Id);
+
+            return BadRequest(new ProblemDetails { Title = "Problem creating order" });
+        }
+    }
+}
--- restore/API/Data/StoreContext.cs
+++ restore/API/Data/StoreContext.cs
@@ -1,11 +1,12 @@
 using API.Entities;
+using API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class StoreContext : IdentityDbContext<User>
+    public class StoreContext : IdentityDbContext<User, Role, int>
     {
         public StoreContext(DbContextOptions options) : base(options)
         {
@@ -13,14 +14,21 @@ namespace API.Data
 
         public DbSet<Product> Products => Set<Product>();
         public DbSet<Basket> Baskets => Set<Basket>();
+        public DbSet<Order> Orders => Set<Order>();
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<IdentityRole>().HasData(
-                new IdentityRole { Name = "Member", NormalizedName = "MEMBER" },
-                new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" }
+            builder.Entity<User>()
+                .HasOne(a => a.Address)
+                .WithOne()
+                .HasForeignKey<UserAddress>(a => a.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Role>().HasData(
+                new Role { Id = 1, Name = "Member", NormalizedName = "MEMBER" },
+                new Role { Id = 2, Name = "Admin", NormalizedName = "ADMIN" }
             );
         }
     }
