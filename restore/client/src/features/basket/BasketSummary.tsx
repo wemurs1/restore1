30d67f20f2a157c6e1abe2b7395320944eff1f1b
Stuart Murray <stuart.murray.dev@outlook.com>
2023-05-25 11:43:57 +0100

    complete basic checkout ui workflow

--- restore/client/src/features/basket/BasketSummary.tsx
+++ restore/client/src/features/basket/BasketSummary.tsx
@@ -1,41 +1,57 @@
-import { Fragment } from 'react';
-import { currencyFormat } from '../../app/util/util';
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+} from '@mui/material';
 import { useAppSelector } from '../../app/store/configureStore';
+import { currencyFormat } from '../../app/util/util';
 
-export default function BasketSummary() {
+interface Props {
+  subtotal?: number;
+}
+
+export default function BasketSummary({ subtotal }: Props) {
   const { basket } = useAppSelector((state) => state.basket);
-
-  if (!basket) return <Fragment></Fragment>;
-
-  const subtotal = basket.items.reduce(
-    (sum, item) => sum + item.price * item.quantity,
-    0
-  );
-
+  if (subtotal === undefined)
+    subtotal =
+      basket?.items.reduce(
+        (sum, item) => sum + item.quantity * item.price,
+        0
+      ) ?? 0;
   const deliveryFee = subtotal > 10000 ? 0 : 500;
 
   return (
-    <Fragment>
-      <div className='d-flex justify-content-between'>
-        <div>Subtotal</div>
-        <div>{currencyFormat(subtotal)}</div>
-      </div>
-      <hr />
-      <div className='d-flex justify-content-between'>
-        <div>Delivery fee *</div>
-        <div>{currencyFormat(deliveryFee)}</div>
-      </div>
-      <hr />
-      <div className='d-flex justify-content-between'>
-        <div>Total</div>
-        <div>{currencyFormat(subtotal + deliveryFee)}</div>
-      </div>
-      <hr />
-      <div className='d-flex justify-content-between'>
-        <div style={{ fontStyle: 'italic' }}>
-          * Orders over $100 qualify for free delivery
-        </div>
-      </div>
-    </Fragment>
+    <>
+      <TableContainer component={Paper} variant={'outlined'}>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell colSpan={2}>Subtotal</TableCell>
+              <TableCell align='right'>{currencyFormat(subtotal)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={2}>Delivery fee*</TableCell>
+              <TableCell align='right'>{currencyFormat(deliveryFee)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={2}>Total</TableCell>
+              <TableCell align='right'>
+                {currencyFormat(subtotal + deliveryFee)}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <span style={{ fontStyle: 'italic' }}>
+                  *Orders over $100 qualify for free delivery
+                </span>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
