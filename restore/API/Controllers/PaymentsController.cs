27078c6ea51a699f748e7e874f3bb62bd2ba511e
Stuart Murray <stuart.murray.dev@outlook.com>
2023-06-01 12:21:23 +0100

    stripe webhook

--- restore/API/Controllers/PaymentsController.cs
+++ restore/API/Controllers/PaymentsController.cs
@@ -1,10 +1,12 @@
 using API.Data;
 using API.DTOs;
+using API.Entities.OrderAggregate;
 using API.Extensions;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 namespace API.Controllers
 {
@@ -12,8 +14,10 @@ namespace API.Controllers
     {
         private readonly IPaymentService _paymentService;
         private readonly StoreContext _context;
-        public PaymentsController(IPaymentService paymentService, StoreContext context)
+        private readonly IConfiguration _config;
+        public PaymentsController(IPaymentService paymentService, StoreContext context, IConfiguration config)
         {
+            _config = config;
             _context = context;
             _paymentService = paymentService;
         }
@@ -40,6 +44,30 @@ namespace API.Controllers
             if (!result) return BadRequest(new ProblemDetails { Title = "Problem updating basket with intent" });
 
             return basket.MapBasketToDto();
+        }
+
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(
+                json,
+                Request.Headers["Stripe-Signature"],
+                _config["StripeSettings:WhSecret"]
+            );
+
+            var charge = (Charge)stripeEvent.Data.Object;
+
+            var order = await _context.Orders.FirstOrDefaultAsync(x => x.PaymentIntentId == charge.PaymentIntentId);
+
+            if (order == null) throw new Exception("Order not found");
+
+            if (charge.Status == "succeeded") order.OrderStatus = OrderStatus.PaymentReceived;
+
+            await _context.SaveChangesAsync();
+
+            return new EmptyResult();
         }
     }
 }
---
+++ restore/stripe.sh
@@ -0,0 +1,1 @@
+stripe listen -f http://localhost:5000/api/payments/webhook -e charge.succeeded
