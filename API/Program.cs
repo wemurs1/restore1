ae7d486f815a30492c8572cfe112169e071dc4ef
Stuart Murray <stuart.murray.dev@outlook.com>
2023-06-05 18:15:19 +0100

    SSL Mode disable on DB

--- API/Program.cs
+++ API/Program.cs
@@ -31,7 +31,7 @@ else
     var pgHost = pgHostPort.Split(":")[0];
     var pgPort = pgHostPort.Split(":")[1];
 
-    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Disable";
 }
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
